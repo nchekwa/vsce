5f225c84191dca04bf031d6a7a4dca03cdcb3bad
Artur Zdolinski <artur@zdolinski.com>
2025-09-05 02:54:11 +0200

    feat: add symlink for docker-compose to system path

--- src/scripts/install_docker.sh
+++ src/scripts/install_docker.sh
@@ -10,3 +10,6 @@ echo "Version Codename: $VERSION_CODENAME"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$OS_ID $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/sbin/docker-compose
