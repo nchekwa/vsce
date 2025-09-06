b219437b13800c2487c5928eb2d635efedbb7d13
Artur Zdolinski <artur@zdolinski.com>
2025-09-06 20:09:03 +0200

    feat: enhance npm installation with latest versions and global packages

--- src/scripts/install_npm.sh
+++ src/scripts/install_npm.sh
@@ -11,7 +11,11 @@ export NVM_DIR="/usr/local/nvm"
 
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 
-nvm install --lts
+nvm install --lts --latest-npm --default
 LTS_VERSION=$(nvm version lts/*)
 nvm alias default $LTS_VERSION
-nvm use default
+export PATH="$NVM_DIR/versions/node/$(nvm version default)/bin:$PATH"
+
+# Ensure npm and npx are available
+npm install -g npm@latest --force
+npm install -g npx@latest --force
